7f30c4bb07ef3f96ce0895f1d7916f2bc9f4c37a
Devarasen <devarasen@gmail.com>
2023-06-26 22:50:54 +1000

    Class color code

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // defining variables
-  var currentDayEl = $("#currentDay");
+$(function () {   
+  currentDayEl = $("#currentDay");
+  
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -14,12 +14,23 @@ $(function () {
 
 
   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // block by comparing the id to the current hour. 
+    function updateBlockClass() {
+      var currentHour = dayjs().format("H");
+      var currentHourInt = parseInt(currentHour);
+      console.log(currentHour);
 
-
+      $(".time-block").each(function () {
+        var timeBlock = parseInt($(this).attr("id").split("-")[1]);
+        if (timeBlock < currentHourInt) {
+          $(this).addClass("past").removeClass("present future");
+        } else if (timeBlock === currentHourInt) {
+          $(this).addClass("present").removeClass("past future");
+        } else {
+          $(this).addClass("future").removeClass("past present");
+        }
+      });
+    }
   
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -28,16 +39,19 @@ $(function () {
 
   // TODO: Add code to display the current date in the header of the page.
   function displayDate() {
-      currentDayEl.text(dayjs().format("dddd, MMM DD[th]"));
+    var currentDay = dayjs().format("dddd, MMM DD[th] mm:ss");
+    currentDayEl.text(currentDay);
   }
-
-  setInterval(displayDate, 1000);
 
   function init() {
     displayDate();
+    updateBlockClass();
   }
+
   init ();
-}
-);
+
+  setInterval(displayDate, 1000);
+  setInterval(updateBlockClass, 60000);
+});
 
 
